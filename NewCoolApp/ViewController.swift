be2b1558a6bbc32681ff752f704a434d0618e989
dstagnaro1 <danielstagnaro@gmail.com>
2016-05-09 21:15:55 -0700

    :)

--- NewCoolApp/ViewController.swift
+++ NewCoolApp/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var monkeyImage: UIImageView!
-    @IBOutlet weak var treeImage: UIImageView!
+    @IBOutlet weak var elephantImage: UIImageView!
+    @IBOutlet weak var frogImage: UIImageView!
+    @IBOutlet weak var catImage: UIImageView!
+    @IBOutlet weak var dogImage: UIImageView!
     
-    @IBOutlet weak var hideMonkeyButton: UIButton!
-    @IBOutlet weak var hideTreeButton: UIButton!
+    @IBOutlet weak var hideElephantButton: UIButton!
+    @IBOutlet weak var hideFrogButton: UIButton!
+    @IBOutlet weak var hideCatButton: UIButton!
+    @IBOutlet weak var hideDogButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,27 +29,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func hideMonkey(sender: AnyObject) {
-        if monkeyImage.hidden == false {
-            monkeyImage.hidden = true
-            hideMonkeyButton.setTitle("Show Monkey", forState: UIControlState.Normal)
+    
+    
+    @IBAction func hideElephant(sender: AnyObject) {
+        if elephantImage.hidden == false {
+            elephantImage.hidden = true
+            hideElephantButton.setTitle("Show Elephant", forState: UIControlState.Normal)
         } else {
-            monkeyImage.hidden = false
-            hideMonkeyButton.setTitle("Hide Monkey", forState: UIControlState.Normal)
-
-        }
-        
-    }
-
-    @IBAction func hideTree(sender: AnyObject) {
-        if treeImage.hidden == false {
-            treeImage.hidden = true
-            hideTreeButton.setTitle("Show Tree", forState: UIControlState.Normal)
-        } else {
-            treeImage.hidden = false
-            hideTreeButton.setTitle("Hide Tree", forState: UIControlState.Normal)
+            elephantImage.hidden = false
+            hideElephantButton.setTitle("Hide Elephant", forState: UIControlState.Normal)
         }
     }
+    @IBAction func hideFrog(sender: AnyObject) {
+        if frogImage.hidden == false {
+            frogImage.hidden = true
+            hideFrogButton.setTitle("Show Frog", forState: UIControlState.Normal)
+        } else {
+            frogImage.hidden = false
+            hideFrogButton.setTitle("Hide Frog", forState: UIControlState.Normal)
+        }
+    }
+    
+    @IBAction func hideCat(sender: AnyObject) {
+        if catImage.hidden == false {
+            catImage.hidden = true
+            hideCatButton.setTitle("Show Cat", forState: UIControlState.Normal)
+        } else {
+            catImage.hidden = false
+            hideCatButton.setTitle("Hide Cat", forState: UIControlState.Normal)
+        }
+    }
+    
+    @IBAction func hideDog(sender: AnyObject) {
+        if dogImage.hidden == false {
+            dogImage.hidden = true
+            hideDogButton.setTitle("Show Dog", forState: UIControlState.Normal)
+        } else {
+            dogImage.hidden = false
+            hideDogButton.setTitle("Hide Dog", forState: UIControlState.Normal)
+        }
+    }
+
 }
 
